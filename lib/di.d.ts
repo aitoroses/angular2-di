a4cf2e63e0e2389cc8eceb1a3709d462daf35942
Aitor Oses <aitor.oses@gmail.com>
2016-01-17 19:17:32 +0100

    Fix the typings

--- lib/di.d.ts
+++ lib/di.d.ts
@@ -1,41 +1,41 @@
 declare module "angular2-di" {
-  export declare var InjectMetadata
-  export declare var OptionalMetadata
-  export declare var InjectableMetadata
-  export declare var SelfMetadata
-  export declare var HostMetadata
-  export declare var SkipSelfMetadata
-  export declare var DependencyMetadata
-  export declare var Inject
-  export declare var Optional
-  export declare var Injectable
-  export declare var Self
-  export declare var Host
-  export declare var SkipSelf
+  export var InjectMetadata
+  export var OptionalMetadata
+  export var InjectableMetadata
+  export var SelfMetadata
+  export var HostMetadata
+  export var SkipSelfMetadata
+  export var DependencyMetadata
+  export var Inject
+  export var Optional
+  export var Injectable
+  export var Self
+  export var Host
+  export var SkipSelf
 
   export interface ForwardRefFn {
       (): any;
   }
-  export declare function forwardRef(forwardRefFn: ForwardRefFn): Function;
-  export declare function resolveForwardRef(type: any): any;
-  export declare var Injector
-  export declare var Binding
-  export declare var ProviderBuilder
-  export declare var ResolvedBinding
-  export declare var ResolvedFactory
-  export declare var Dependency
-  export declare var bind
-  export declare var Provider
-  export declare var ResolvedProvider
-  export declare var provide
-  export declare var Key
-  export declare var TypeLiteral
-  export declare var NoProviderError
-  export declare var AbstractProviderError
-  export declare var CyclicDependencyError
-  export declare var InstantiationError
-  export declare var InvalidProviderError
-  export declare var NoAnnotationError
-  export declare var OutOfBoundsError
-  export declare var OpaqueToken
+  export function forwardRef(forwardRefFn: ForwardRefFn): Function;
+  export function resolveForwardRef(type: any): any;
+  export var Injector
+  export var Binding
+  export var ProviderBuilder
+  export var ResolvedBinding
+  export var ResolvedFactory
+  export var Dependency
+  export var bind
+  export var Provider
+  export var ResolvedProvider
+  export var provide
+  export var Key
+  export var TypeLiteral
+  export var NoProviderError
+  export var AbstractProviderError
+  export var CyclicDependencyError
+  export var InstantiationError
+  export var InvalidProviderError
+  export var NoAnnotationError
+  export var OutOfBoundsError
+  export var OpaqueToken
 }
